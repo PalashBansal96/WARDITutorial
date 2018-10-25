fac6eb172abe10085a12ed8e389f8d2f2edeb66f
Palash Bansal <palashbansal96@gmail.com>
2018-10-26 04:47:35 +0530

    Camera following player movement

---
+++ Assets/Scenes/Scripts/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+	public Transform _player;
+
+	private Vector3 _startDelta;
+	// Use this for initialization
+	void Start ()
+	{
+		_startDelta = _player.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = _player.position - _startDelta;
+	}
+}
