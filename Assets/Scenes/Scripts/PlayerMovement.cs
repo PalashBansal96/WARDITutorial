4258435a370d0ce14d172f737236fb997265ada2
Palash Bansal <palashbansal96@gmail.com>
2018-10-26 04:22:35 +0530

    Basic player movement

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
+	private Rigidbody _rigidbody;
+	public float _acceleration = 1f;  //in ms^-2
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_rigidbody = GetComponent<Rigidbody>(); // Getting the attached Rigidbody component
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		//Basic movement with WASD
+		if (Input.GetKey(KeyCode.A))
+			_rigidbody.AddForce(-1*_acceleration * Time.deltaTime, 0, 0); //Note: acceleration is multiplied by Time.deltaTime to account for fps changes.
+		if (Input.GetKey(KeyCode.D))
+			_rigidbody.AddForce(_acceleration * Time.deltaTime, 0, 0);
+		if (Input.GetKey(KeyCode.S))
+			_rigidbody.AddForce(0, 0, -1*_acceleration * Time.deltaTime);
+		if (Input.GetKey(KeyCode.W))
+			_rigidbody.AddForce(0, 0, _acceleration * Time.deltaTime);
+
+		//Jumping
+		if (Input.GetKeyDown(KeyCode.Space))
+			_rigidbody.AddForce(0, _acceleration, 0);
+
 	}
 }
