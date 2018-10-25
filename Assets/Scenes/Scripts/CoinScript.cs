9708d0c78d274f96e0738d2d71ee4939189afa29
Palash Bansal <palashbansal96@gmail.com>
2018-10-26 04:41:25 +0530

    Coin prefab and collection

---
+++ Assets/Scenes/Scripts/CoinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+	//We don't need the start and edit functions here
+	
+	
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.gameObject.GetComponent<ScoreManager>().score += 1;
+			print("Coin collected");
+			Destroy(gameObject); //Destroy self.
+		}
+	}
+
+}
---
+++ Assets/Scenes/Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	//To be attached to the player object 
+	public int score;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		score = 0;
+	}
+}
